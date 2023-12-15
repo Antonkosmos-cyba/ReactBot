6330a8e65b19aad4db6dceca5f09b278d4ece9a0
Antonkosmos-cyba <natalijatsyba@yandex.ru>
2023-12-15 13:12:12 +0300

    start bootsrap

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React from "react";
 
 // import logo from "./logo.svg";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { NaviBar } from "./components/Navbar";
 
 function App() {
-  return <div className="App">nanaaaa</div>;
+  return (
+    <div className="App">
+      <NaviBar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Navbar } from "react-bootstrap";
+
+export function NaviBar() {
+  return (
+    <>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" />
+    </>
+  );
+}
