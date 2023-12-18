202346c30da57a92865b82c66119bbe2c4b99646
Antonkosmos-cyba <natalijatsyba@yandex.ru>
2023-12-18 14:14:16 +0300

    nav

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import { NaviBar } from "./components/navbar/Navbar.jsx";
+import NaviBar from "./components/navbar/Navbar.jsx";
 
 function App() {
   return (
