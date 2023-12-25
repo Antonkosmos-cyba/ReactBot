cb3a761b2d4dcb84a9ba6fb30c52f10d52c268e2
Antonkosmos-cyba <natalijatsyba@yandex.ru>
2023-12-25 14:29:57 +0300

    nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,28 +1,41 @@
 import React from "react";
 // import { Link } from "react";
 import "./navbar.css";
-// import { Route, Routes } from "react-router-dom";
-// import Video from "../pages/video/Video.jsx";
-// import Music from "../pages/music/Music.jsx";
-// import Main from "../pages/main/Main.jsx";
+import { Route, Routes } from "react-router-dom";
+import Video from "../pages/video/Video.jsx";
+import Music from "../pages/music/Music.jsx";
+import Main from "../pages/main/Main.jsx";
 
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 
 function Heder() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="md" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand className="text-logo">Новый 2024 год</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link>
+              <Link to="/"> Главная</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/music">Музыка</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/video">Видео</Link>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
+      <Routes>
+        <Route exact path="/" element={<Main />}></Route>
+        <Route path="/music" element={<Music />}></Route>
+        <Route path="/video" element={<Video />}></Route>
+      </Routes>
     </Navbar>
   );
 }
