f2e69f73197bb38e42b2d427ab97e6c03d758187
Antonkosmos-cyba <natalijatsyba@yandex.ru>
2024-01-07 16:53:17 +0300

    foto

--- src/components/pages/music/Music.jsx
+++ src/components/pages/music/Music.jsx
@@ -2,27 +2,47 @@ import React from "react";
 import "./music.css";
 
 import Carousel from "react-bootstrap/Carousel";
-import ExampleCarouselImage from "components/ExampleCarouselImage";
+import Image1 from "./milana2.jpeg";
+import Image2 from "./di2.webp";
+import Image3 from "./Nuzhnye2.jpg";
 
 function Music() {
   return (
-    <Carousel fade>
+    <Carousel fade className="positem">
       <Carousel.Item>
-        <ExampleCarouselImage text="First slide" />
+        <img
+          src={Image1}
+          alt=""
+          // max-width={600}
+          // height={300}
+          className="pos"
+        ></img>
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage text="Second slide" />
+        <img
+          src={Image2}
+          alt=""
+          // max-width={400}
+          // height={200}
+          className="pos"
+        ></img>
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage text="Third slide" />
+        <img
+          src={Image3}
+          alt=""
+          // max-width={400}
+          // height={200}
+          className="pos"
+        ></img>
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
