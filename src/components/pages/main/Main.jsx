ffc25d58af2ff8794f3359f6358d7a33ee8436b4
Antonkosmos-cyba <natalijatsyba@yandex.ru>
2024-01-21 15:37:45 +0300

    ram

--- src/components/pages/main/Main.jsx
+++ src/components/pages/main/Main.jsx
@@ -1,22 +1,40 @@
 import React from "react";
 import "./main.css";
+import Ram from "./horizontally-centered-vertical-decoration.png";
 
 function Main() {
   return (
     <>
-      <p>
-        Мы рады сообщить Вам, что 26 июня 2024 года состоится праздничный вечер,
-        посвященный нашему
-        <br /> бракосочетанию.
-        <br />
-        Мы от всего сердца просим Вас провести этот незабываемый день вместе с
-        нами.
-        <br />
-        Иван и Елизавета.
-        <br />
-        Праздник состоится по адресу: ул. Вершина, д. 24 кв. 26
-        <br />в 18.00
-      </p>
+      <div class="body">
+        <div class="outer-border">
+          <div class="mid-border">
+            <div class="inner-border">
+              <img class="vertical-decoration top" src={Ram}></img>
+              <img class="vertical-decoration bottom" src={Ram}></img>
+
+              <div class="container">
+                <div class="row">
+                  <div class="col-lg-12 text-center">
+                    <p>
+                      Мы рады сообщить Вам, что 26 июня 2024 года состоится
+                      праздничный вечер, посвященный нашему
+                      <br /> бракосочетанию.
+                      <br />
+                      Мы от всего сердца просим Вас провести этот незабываемый
+                      день вместе с нами.
+                      <br />
+                      Иван и Елизавета.
+                      <br />
+                      Праздник состоится по адресу: ул. Вершина, д. 24 кв. 26
+                      <br />в 18.00
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
