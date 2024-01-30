b395776ec059ef7696035e0f5b1187889a185098
Antonkosmos-cyba <natalijatsyba@yandex.ru>
2024-01-30 15:18:52 +0300

    cards muz

--- src/components/pages/audio/Playlist.jsx
+++ src/components/pages/audio/Playlist.jsx
@@ -1,21 +1,40 @@
+import Image1 from "./milana5.png";
+import Image2 from "./di5.png";
+import Image3 from "./Nuzhnye5.jpg";
+import Image4 from "./vika.webp";
+import muzik1 from "./Milana.mp3";
+import muzik2 from "./di.mp3";
+import muzik3 from "./Milana.mp3";
+import muzik4 from "./Milana.mp3";
+
 const audios = [
   {
+    img: Image1,
     name: "Shaman",
     song: "Я русский",
-    melody: "rus",
+    muz: muzik1,
     id: 1,
   },
   {
+    img: Image2,
     name: "Shamansky",
     song: "Я белорусский",
-    melody: "belorus",
+    muz: muzik2,
     id: 2,
   },
   {
+    img: Image3,
     name: "Shamanec",
     song: "Я чеченский",
-    melody: "chechen",
+    muz: muzik3,
     id: 3,
+  },
+  {
+    img: Image4,
+    name: "Вика старикова",
+    song: "Я чеченский",
+    muz: muzik4,
+    id: 4,
   },
 ];
 export default audios;
