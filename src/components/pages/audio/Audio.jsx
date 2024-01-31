01158f0fa9b1dae96796b05b166cef781991c8aa
Antonkosmos-cyba <natalijatsyba@yandex.ru>
2024-01-31 17:59:16 +0300

    audio

--- src/components/pages/audio/Audio.jsx
+++ src/components/pages/audio/Audio.jsx
@@ -2,24 +2,36 @@ import React from "react";
 import "./audio.css";
 import audios from "./Playlist";
 import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 function Audio() {
   return (
     <>
-      {audios.map((audio) => {
-        return (
-          <div className="cont">
-            <Card className="cb" style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={audio.img} />
-              <Card.Body>
-                <Card.Title>{audio.name}</Card.Title>
-                <Card.Text className="pp">{audio.song}</Card.Text>
-                <audio className="player" controls src={audio.muz}></audio>
-              </Card.Body>
-            </Card>
-          </div>
-        );
-      })}
+      <div className="section">
+        <div className="cont">
+          <Row xs={1} sm={2} md={2} xl={6} xxl={4} className="g-4 ">
+            {audios.map((audio) => {
+              return (
+                <Col>
+                  <Card className="cb" style={{ width: "18rem" }}>
+                    <Card.Img variant="top" src={audio.img} />
+                    <Card.Body>
+                      <Card.Title>{audio.name}</Card.Title>
+                      <Card.Text className="pp">{audio.song}</Card.Text>
+                      <audio
+                        className="player"
+                        controls
+                        src={audio.muz}
+                      ></audio>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              );
+            })}
+          </Row>
+        </div>
+      </div>
     </>
   );
 }
