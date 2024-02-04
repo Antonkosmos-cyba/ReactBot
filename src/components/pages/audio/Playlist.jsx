141a84d83c0366b15399057c41db1235612d721a
Antonkosmos-cyba <natalijatsyba@yandex.ru>
2024-02-04 22:43:57 +0300

    muz

--- src/components/pages/audio/Playlist.jsx
+++ src/components/pages/audio/Playlist.jsx
@@ -4,35 +4,35 @@ import Image3 from "./Nuzhnye5.jpg";
 import Image4 from "./vika1.webp";
 import muzik1 from "./Milana.mp3";
 import muzik2 from "./di.mp3";
-import muzik3 from "./Milana.mp3";
-import muzik4 from "./Milana.mp4";
+import muzik3 from "./nuz.mp3";
+import muzik4 from "./vka.mp3";
 
 const audios = [
   {
     img: Image1,
-    name: "Shaman",
-    song: "Я русский",
+    name: "Милана Филимонова",
+    song: "Всё будет хорошо!",
     muz: muzik1,
     id: 1,
   },
   {
     img: Image2,
-    name: "Shamansky",
-    song: "Я белорусский",
+    name: "Диляра Ди",
+    song: "Аккаунт забанен в соцсети",
     muz: muzik2,
     id: 2,
   },
   {
     img: Image3,
-    name: "Shamanec",
-    song: "Я чеченский",
+    name: "Нужные люди",
+    song: "За нами страна",
     muz: muzik3,
     id: 3,
   },
   {
     img: Image4,
     name: "Вика старикова",
-    song: "Я адыгский",
+    song: "три желания",
     muz: muzik4,
     id: 4,
   },
