dacb0707ce3b4dd6d795fbb6f89fd47f661d44b1
Antonkosmos-cyba <natalijatsyba@yandex.ru>
2024-02-08 15:30:39 +0300

    audio

--- src/components/pages/main/Main.jsx
+++ src/components/pages/main/Main.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./main.scss";
+import magu from "./magu.mp3";
+import video from "./ant.mp4";
 
 function Main() {
   return (
     <>
-      <p>nanana</p>
+      <div className="blog">
+        <audio controls src={magu}></audio>
+        <div></div>
+        <video controls width={400} height={300} src={video}></video>
+      </div>
     </>
   );
 }
